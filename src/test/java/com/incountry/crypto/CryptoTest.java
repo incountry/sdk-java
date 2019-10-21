13b6fb128a5023e50c8fabfc26c553dd9ac03fb5
avdyakovs <sergey.avdyakov@incountry.com>
2019-10-21 14:39:15 +0300

    EN-997: Add test for v1 decryption

--- src/test/java/com/incountry/crypto/CryptoTest.java
+++ src/test/java/com/incountry/crypto/CryptoTest.java
@@ -40,4 +40,12 @@ public class CryptoTest {
         assertEquals(decrypted, "InCountry");
     }
 
+    @Test
+    public void testV1Decryption() throws GeneralSecurityException, IOException {
+        Crypto crypto = new Crypto("password");
+        String encrypted = "1:8b02d29be1521e992b49a9408f2777084e9d8195e4a3392c68c70545eb559670b70ec928c8eeb2e34f118d32a23d77abdcde38446241efacb71922579d1dcbc23fca62c1f9ec5d97fbc3a9862c0a9e1bb630aaa3585eac160a65b24a96af5becef3cdc2b29";
+        String decrypted = crypto.decrypt(encrypted);
+        assertEquals(decrypted, "InCountry");
+    }
+
 }
