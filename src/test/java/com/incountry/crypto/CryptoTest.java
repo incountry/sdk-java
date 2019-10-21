f5b887ffd277a3eda06c020606a83495685f937e
avdyakovs <sergey.avdyakov@incountry.com>
2019-10-21 15:49:58 +0300

    EN-997: Change assert parameters order in tests to match (expected, actual) pattern

--- src/test/java/com/incountry/crypto/CryptoTest.java
+++ src/test/java/com/incountry/crypto/CryptoTest.java
@@ -27,8 +27,8 @@ public class CryptoTest {
         for (String plainText: plainTexts) {
             String encrypted = crypto.encrypt(plainText);
             String decrypted = crypto.decrypt(encrypted);
-            assertEquals(decrypted, plainText);
-            assertNotEquals(encrypted, plainText);
+            assertEquals(plainText, decrypted);
+            assertNotEquals(plainText, encrypted);
         }
     }
 
@@ -37,7 +37,7 @@ public class CryptoTest {
         Crypto crypto = new Crypto("password");
         String encrypted = "7765618db31daf5366a6fc3520010327";
         String decrypted = crypto.decrypt(encrypted);
-        assertEquals(decrypted, "InCountry");
+        assertEquals("InCountry", decrypted);
     }
 
     @Test
@@ -45,7 +45,7 @@ public class CryptoTest {
         Crypto crypto = new Crypto("password");
         String encrypted = "1:8b02d29be1521e992b49a9408f2777084e9d8195e4a3392c68c70545eb559670b70ec928c8eeb2e34f118d32a23d77abdcde38446241efacb71922579d1dcbc23fca62c1f9ec5d97fbc3a9862c0a9e1bb630aaa3585eac160a65b24a96af5becef3cdc2b29";
         String decrypted = crypto.decrypt(encrypted);
-        assertEquals(decrypted, "InCountry");
+        assertEquals("InCountry", decrypted);
     }
 
 }
