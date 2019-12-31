b43623cd7dc6da8ee26d4664f9dc54463ebdafb6
robert-par <robert.paronyan@incountry.com>
2019-12-31 13:12:20 +0300

    EN-1594 add migration script example

---
+++ src/examples/java/com/incounty/FullMIgration.java
@@ -0,0 +1,48 @@
+package com.incounty;
+
+import com.incountry.MigrateResult;
+import com.incountry.Storage;
+import com.incountry.exceptions.FindOptionsException;
+import com.incountry.exceptions.StorageException;
+import com.incountry.keyaccessor.SecretKeyAccessor;
+import com.incountry.keyaccessor.generator.SecretKeyGenerator;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class FullMIgration {
+
+    private final String COUNTRY = "us";
+
+    public void startMIgration() throws IOException, StorageException, GeneralSecurityException, FindOptionsException {
+
+        SecretKeyAccessor secretKeyAccessor = SecretKeyAccessor.getAccessor(new SecretKeyGenerator<String>() {
+            @Override
+            public String generate() {
+                return "        {\n" +
+                        "            \"currentVersion\": 1,\n" +
+                        "            \"secrets\": [\n" +
+                        "                {\"secret\": \"password0\", \"version\": 0},\n" +
+                        "                {\"secret\": \"password1\", \"version\": 1},\n" +
+                        "            ],\n" +
+                        "        }";
+            }
+        });
+
+        Storage storage = new Storage(
+                "envId",
+                "apiKey",
+                secretKeyAccessor
+
+        );
+
+        boolean migrationComplete = false;
+        while (!migrationComplete) {
+            MigrateResult migrationResult = storage.migrate(COUNTRY, 50);
+            if (migrationResult.getTotalLeft() == 0) {
+                migrationComplete = true;
+            }
+        }
+
+    }
+}
