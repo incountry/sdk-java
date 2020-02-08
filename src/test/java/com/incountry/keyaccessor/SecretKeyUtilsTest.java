a54a4de34c4a3a447fd50cbce8b9b2b1948202bb
robert-par <robert.paronyan@incountry.com>
2020-02-08 18:21:46 +0300

    EN-2125 added convertStringToSecretKeyData test

---
+++ src/test/java/com/incountry/keyaccessor/SecretKeyUtilsTest.java
@@ -0,0 +1,48 @@
+package com.incountry.keyaccessor;
+
+import com.google.gson.Gson;
+import com.incountry.keyaccessor.key.SecretKey;
+import com.incountry.keyaccessor.key.SecretKeysData;
+import com.incountry.keyaccessor.utils.SecretKeyUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SecretKeyUtilsTest {
+
+    private String secretKeyString;
+
+    private String secret = "user_password";
+    private int version = 0;
+    private boolean isKey = true;
+    private int currentVersion = 0;
+
+    @Before
+    public void init() {
+        SecretKeysData secretKeysData = new SecretKeysData();
+        SecretKey secretKey = new SecretKey();
+        secretKey.setSecret(secret);
+        secretKey.setVersion(version);
+        secretKey.setIsKey(true);
+        List<SecretKey> secretKeyList = new ArrayList<>();
+        secretKeyList.add(secretKey);
+        secretKeysData.setSecrets(secretKeyList);
+        secretKeysData.setCurrentVersion(0);
+
+        secretKeyString = new Gson().toJson(secretKeysData);
+    }
+
+    @Test
+    public void testConvertStringToSecretKeyData(){
+        SecretKeysData secretKeysData = SecretKeyUtils.convertStringToSecretKeyData(secretKeyString);
+        assertEquals(currentVersion, secretKeysData.getCurrentVersion());
+        assertEquals(secret, secretKeysData.getSecrets().get(0).getSecret());
+        assertEquals(version, secretKeysData.getSecrets().get(0).getVersion());
+        assertEquals(isKey, secretKeysData.getSecrets().get(0).getIsKey());
+    }
+
+}
