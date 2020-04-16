d3d80ee2c1bfb54fcc18ae887e2fbf1b75e6ba5a
vladimir.levitskiy <vladimir.levitsky@incountry.com>
2020-04-16 16:28:17 +0300

    SDK-50 fix sonar recomendations

--- src/main/java/com/incountry/residence/sdk/tools/keyaccessor/generator/SecretKeyGenerator.java
+++ src/main/java/com/incountry/residence/sdk/tools/keyaccessor/generator/SecretKeyGenerator.java
@@ -1,5 +1,5 @@
 package com.incountry.residence.sdk.tools.keyaccessor.generator;
 
-public interface SecretKeyGenerator<T> {
-    T generate();
+public interface SecretKeyGenerator {
+    Object generate();
 }
