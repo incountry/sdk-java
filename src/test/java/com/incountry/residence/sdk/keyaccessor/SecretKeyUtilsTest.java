2058dd586e244d3763b2eb94add22413ca7429c5
vladimir.levitskiy <vladimir.levitsky@incountry.com>
2020-04-16 17:01:07 +0300

    SDK-51 refactor after merging with develop branch

--- src/test/java/com/incountry/residence/sdk/keyaccessor/SecretKeyUtilsTest.java
+++ src/test/java/com/incountry/residence/sdk/keyaccessor/SecretKeyUtilsTest.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.incountry.residence.sdk.tools.JsonUtils;
 import com.incountry.residence.sdk.tools.keyaccessor.SecretKeyAccessor;
-import com.incountry.residence.sdk.tools.keyaccessor.generator.SecretKeyGenerator;
 import com.incountry.residence.sdk.tools.keyaccessor.key.SecretKey;
 import com.incountry.residence.sdk.tools.keyaccessor.key.SecretKeysData;
 import com.incountry.residence.sdk.tools.keyaccessor.utils.SecretKeyUtils;
@@ -107,7 +106,7 @@ public class SecretKeyUtilsTest {
                 "}";
 
         assertThrows(IllegalArgumentException.class,
-                () -> SecretKeyAccessor.getAccessor((SecretKeyGenerator<String>) () -> secretDataWrongJson));
+                () -> SecretKeyAccessor.getAccessor(() -> secretDataWrongJson));
     }
 
 }
