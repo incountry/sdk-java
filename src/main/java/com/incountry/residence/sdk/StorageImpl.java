3dc404b427bcd8cdd1d5b992f70c093c7c8741f9
vladimir.levitskiy <vladimir.levitsky@incountry.com>
2020-09-02 20:17:39 +0300

    SDK-219 fix bug at 'migration'

--- src/main/java/com/incountry/residence/sdk/StorageImpl.java
+++ src/main/java/com/incountry/residence/sdk/StorageImpl.java
@@ -287,8 +287,12 @@ public class StorageImpl implements Storage {
                 .limitAndOffset(limit, 0)
                 .keyNotEq(StringField.VERSION, String.valueOf(cryptoManager.getCurrentSecretVersion()));
         BatchRecord batchRecord = find(country, builder);
-        batchWrite(country, batchRecord.getRecords());
-        MigrateResult result = new MigrateResult(batchRecord.getCount(), batchRecord.getTotal() - batchRecord.getCount());
+        if (batchRecord.getCount() > 0) {
+            batchWrite(country, batchRecord.getRecords());
+        }
+        MigrateResult result = new MigrateResult(batchRecord.getCount(),
+                batchRecord.getTotal() - batchRecord.getCount(),
+                batchRecord.getErrors());
         if (LOG.isTraceEnabled()) {
             LOG.trace("batchWrite results={}", result);
         }
